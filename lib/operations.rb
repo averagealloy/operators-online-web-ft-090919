6c39f0d6df4ac7cfc94a2a0a4334f5a04f53664f
Mike McTighe <m.mctighe10@gmail.com>
2019-09-09 19:54:02 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -9,17 +9,6 @@ end
 end
 
 
-#def not_safe?(speed)
-#if speed > 60
-#  true
-#elsif speed < 40
-#  true
-#else speed > 40 && speed < 60
-#  false
-#end
-#end
-
-
 def not_safe?(speed)
   speed > 60 ? true : false
   if speed > 40 && speed < 60
